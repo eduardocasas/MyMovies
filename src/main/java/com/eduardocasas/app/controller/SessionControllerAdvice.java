dea531d5bff37bd4e7aa38c7a7dd05e74cdd6fa5
eduardocasas <eduardo.casas.lopez@gmail.com>
2014-05-24 07:45:10 +0200

    clean temp files and refactor vendor web files

--- src/main/java/com/eduardocasas/app/controller/SessionControllerAdvice.java
+++ src/main/java/com/eduardocasas/app/controller/SessionControllerAdvice.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
  * @since       2014-05-23
  */
 @ControllerAdvice
-public class GlobalControllerAdvice {
+public class SessionControllerAdvice {
 
     private HttpSession session;
     private HttpServletRequest request;
@@ -21,12 +21,12 @@ public class GlobalControllerAdvice {
     public void  myMethod(HttpSession session, HttpServletRequest request, HttpServletResponse response) throws Exception {
         this.session = session;
         this.request = request;
-        if (userIsAuthorized()) {
+        if (userIsNotAuthorized()) {
             response.sendRedirect("/session/login");
         }
     }
 
-    private Boolean userIsAuthorized() {
+    private Boolean userIsNotAuthorized() {
         return (session.getAttribute("login") == null || !(Boolean)session.getAttribute("login")) && !request.getRequestURI().contains("session");
     }
 
